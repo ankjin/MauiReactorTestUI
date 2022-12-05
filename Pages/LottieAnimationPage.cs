6cef2c380e78dffe65db48502793985f213dd433
flexi <felixm.jr01@gmail.com>
2022-12-05 11:27:25 +0300

    - update base on ado's code

--- Pages/LottieAnimationPage.cs
+++ Pages/LottieAnimationPage.cs
@@ -4,8 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MauiReactor;
+using SkiaSharp.Extended.UI.Controls;
 
 namespace MauiReactorTestUI.Pages;
+
+
+[Scaffold(typeof(SkiaSharp.Extended.UI.Controls.SKSurfaceView))]
+partial class SKSurfaceView { }
+
+[Scaffold(typeof(SkiaSharp.Extended.UI.Controls.SKAnimatedSurfaceView))]
+partial class SKAnimatedSurfaceView { }
 
 [Scaffold(typeof(SkiaSharp.Extended.UI.Controls.SKLottieView))]
 partial class SKLottieView { }
@@ -20,10 +28,14 @@ class LottieAnimationPage : Component
             {
                 new VerticalStackLayout
                 {
-                    new SKLottieView("dotnetbot.json")
-                        .RepeatCount(1)
-                        .HeightRequest(20)
-                        .WidthRequest(20)
+                    new SKLottieView()
+                        .Source(new SKFileLottieImageSource
+                        {
+                            File = "dotnetbot.json",
+                        })
+                        .RepeatCount(-1)
+                        .HeightRequest(100)
+                        .WidthRequest(100)
                         .HCenter()
                 }
                 .VCenter()
@@ -32,4 +44,5 @@ class LottieAnimationPage : Component
             }
         };
     }
+
 }
